abbec37e35d2369ce3bee357d40749204e8cff0d
mithun <mithunpaul08@gmail.com>
2016-10-01 11:41:40 -0700

    changed username pwd to generic

--- jennyLogin.sh
+++ jennyLogin.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/expect -f
-spawn ssh mithunpaul@lectura.cs.arizona.edu
+spawn ssh yourusername@lectura.cs.arizona.edu
 expect "password"
-send "Alohomora123+\r"
+send "yourPassword\r"
 expect "$ "
 send "whoami\r"
 expect "$ "
-send "ssh mithunpaul@jenny.cs.arizona.edu\r"
+send "ssh yourusername@jenny.cs.arizona.edu\r"
 expect "password"
-send "Alohomora123+\r"
+send "yourpassword\r"
 expect "$ "
 send "whoami\r"
 expect "$ "
